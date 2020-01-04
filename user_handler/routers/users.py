11434e9d290629b0181ffc049de042c9a0de490a
Timotej Gale <timotej.gale@gmail.com>
2020-01-05 00:13:22 +0100

    Fix paths

--- user_handler/routers/users.py
+++ user_handler/routers/users.py
@@ -13,7 +13,7 @@ except:
 router = APIRouter()
 
 
-@router.post('/users/', response_model=schemas.User)
+@router.post('/users', response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_username(db, username=user.username)
     if db_user:
@@ -21,7 +21,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
-@router.get('/users/', response_model=List[schemas.User])
+@router.get('/users', response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
@@ -35,7 +35,7 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@router.post('/login/', response_model=schemas.User)
+@router.post('/login', response_model=schemas.User)
 def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_username_password(db, user=user)
     if db_user:
